a3c04d67e09ea22762d55ed975517386d08f1f30
Sam Lee-Lindsay <sleelin@gmail.com>
2015-05-11 12:58:58 +1000

    Skip files with empty contents

--- ngmod-stylesheet/index.js
+++ ngmod-stylesheet/index.js
@@ -22,77 +22,81 @@ module.exports.create = function () {
         modules = [];
 
     return through.ctor({objectMode: true, verbose: true, read: true, immediate: false}, function transform(file, enc, next) {
-        var stream = this;
+        if (gutil.isNull(file.contents) || file.contents.toString() === "") {
+            next();
+        } else {
+            var stream = this;
 
-        try {
-            // Parse source file contents and traverse AST, searching for angular module definitions
-            astra(esprima.parse(file.contents.toString()), true).when({
-                type: "CallExpression",
-                callee: {
-                    type: "MemberExpression",
-                    object: {type: "Identifier", name: "angular"},
-                    property: {type: "Identifier", name: "module"}
-                },
-                arguments: []
-            }, function (chunk, next) {
-                if ((chunk.arguments.length === 2) && (_.chain(modules).pluck("path").value().indexOf(file.path) < 0)) {
-                    modules[modules.push(file) - 1].ngmodule = chunk.arguments[0].value;
-                }
+            try {
+                // Parse source file contents and traverse AST, searching for angular module definitions
+                astra(esprima.parse(file.contents.toString()), true).when({
+                    type: "CallExpression",
+                    callee: {
+                        type: "MemberExpression",
+                        object: {type: "Identifier", name: "angular"},
+                        property: {type: "Identifier", name: "module"}
+                    },
+                    arguments: []
+                }, function (chunk, next) {
+                    if ((chunk.arguments.length === 2) && (_.chain(modules).pluck("path").value().indexOf(file.path) < 0)) {
+                        modules[modules.push(file) - 1].ngmodule = chunk.arguments[0].value;
+                    }
 
-                next();
-            }).run(function () {
-                var paths = _.chain(modules).pluck("relative").map(path.dirname);
+                    next();
+                }).run(function () {
+                    var paths = _.chain(modules).pluck("relative").map(path.dirname);
 
-                merge(modules.map(function (file) {
-                    var relativeDir = path.dirname(file.relative),
-                        files = [];
+                    merge(modules.map(function (file) {
+                        var relativeDir = path.dirname(file.relative),
+                            files = [];
 
-                    // Look for all CSS files which are siblings or descendants of this module
-                    return fs.src([path.join("**", "*.css")].concat(exclude(paths.without(relativeDir), relativeDir)), {
-                        cwd: path.dirname(file.path),
-                        base: path.dirname(file.path)
-                    }).pipe(map(function (file, next) {
-                        files.push(file);
-                        next();
-                    })).on("end", function () {
-                        if (files.length > 0) {
-                            // Create the module stylesheet file
-                            file = file.clone({contents: false});
-                            file.path = path.join(path.dirname(file.path), [path.basename(file.path, ".js"), "css"].join("."));
-                            file.contents = Buffer.concat(_.pluck(files, "contents"));
-                            file.stylesheets = files;
+                        // Look for all CSS files which are siblings or descendants of this module
+                        return fs.src([path.join("**", "*.css")].concat(exclude(paths.without(relativeDir), relativeDir)), {
+                            cwd: path.dirname(file.path),
+                            base: path.dirname(file.path)
+                        }).pipe(map(function (file, next) {
+                            files.push(file);
+                            next();
+                        })).on("end", function () {
+                            if (files.length > 0) {
+                                // Create the module stylesheet file
+                                file = file.clone({contents: false});
+                                file.path = path.join(path.dirname(file.path), [path.basename(file.path, ".js"), "css"].join("."));
+                                file.contents = Buffer.concat(_.pluck(files, "contents"));
+                                file.stylesheets = files;
 
-                            // Update the module's file if it already exists, or add the module
-                            if (_.chain(stylesheets).where({path: file.path}).value().length > 0) {
-                                stylesheets[_.pluck(stylesheets, "path").indexOf(file.path)] = file;
-                            } else {
-                                stylesheets.push(file);
+                                // Update the module's file if it already exists, or add the module
+                                if (_.chain(stylesheets).where({path: file.path}).value().length > 0) {
+                                    stylesheets[_.pluck(stylesheets, "path").indexOf(file.path)] = file;
+                                } else {
+                                    stylesheets.push(file);
 
-                                if (stream.options.verbose && stream.options.read) {
-                                    gutil.log("Building stylesheet", gutil.colors.magenta(file.relative), "for AngularJS module", gutil.colors.cyan(file.ngmodule));
+                                    if (stream.options.verbose && stream.options.read) {
+                                        gutil.log("Building stylesheet", gutil.colors.magenta(file.relative), "for AngularJS module", gutil.colors.cyan(file.ngmodule));
+                                    }
                                 }
                             }
-                        }
+                        });
+                    })).on("end", function () {
+                        next(null, file);
                     });
-                })).on("end", function () {
-                    next(null, file);
                 });
-            });
-        } catch (ex) {
-            // Not a JavaScript file, so must be a stylesheet.
-            // See if it was picked up earlier and needs updating
-            findExisting(file, function (stylesheet, index) {
-                // File is part of a module's stylesheet, replace reference and rebuild module stylesheet
-                stylesheet.stylesheets[index] = file.clone();
-                stylesheet.contents = Buffer.concat(_.pluck(stylesheet.stylesheets, "contents"));
+            } catch (ex) {
+                // Not a JavaScript file, so must be a stylesheet.
+                // See if it was picked up earlier and needs updating
+                findExisting(file, function (stylesheet, index) {
+                    // File is part of a module's stylesheet, replace reference and rebuild module stylesheet
+                    stylesheet.stylesheets[index] = file.clone();
+                    stylesheet.contents = Buffer.concat(_.pluck(stylesheet.stylesheets, "contents"));
 
-                // Emit latest stylesheet
-                stream.push(stylesheet);
+                    // Emit latest stylesheet
+                    stream.push(stylesheet);
 
-                if (stream.options.verbose) gutil.log("Rebuilding stylesheet", gutil.colors.magenta(stylesheet.relative));
-            });
+                    if (stream.options.verbose) gutil.log("Rebuilding stylesheet", gutil.colors.magenta(stylesheet.relative));
+                });
 
-            next();
+                next();
+            }
         }
     }, function flush(next) {
         // Flush will only run if the stream ends
@@ -121,4 +125,4 @@ module.exports.create = function () {
             return ["!", filepath, path.sep, "**"].join("");
         }).value();
     }
-}
+};
